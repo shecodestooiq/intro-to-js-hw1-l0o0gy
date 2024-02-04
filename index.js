fe0ada5346b45ead656216e549b2827c93206f85
l0o0gy <minamahone9620@gmail.com>
2024-02-05 00:16:46 +0300

    homework1

---
+++ index.js
@@ -0,0 +1,25 @@
+var myName ="logy";
+console.log(myName);
+console.log("type of myName", typeof myName);
+
+var age = 24;
+console.log(age);
+console.log("type of my Age is",typeof age);
+
+
+var isStusent = true
+console.log(isStusent);
+console.log("type of isStudent is", typeof isStusent);
+
+var number1 =60;
+var number2 =10;
+console.log("sum",number1+number2);
+console.log("divition",number1/ number2);
+console.log("divition",number2/ number1);
+console.log("multip",number1*number2);
+console.log("subs",number1-number2);
+console.log("subs",number2-number1);
+
+var greeting="hello";
+var Name ="logy";
+console.log(greeting,Name);
